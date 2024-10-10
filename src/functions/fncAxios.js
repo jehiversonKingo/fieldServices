e7163190544f9d540e38003ef13e95fe0f656101
carlos-lara-dev <carlos.lara.developer@gmail.com>
2024-10-10 12:24:39 -0600

    refactor handshake messages

--- src/functions/fncAxios.js
+++ src/functions/fncAxios.js
@@ -2,22 +2,22 @@ import axios from 'axios';
 import auth from '@react-native-firebase/auth';
 
 export const axiosInstance = axios.create({
-  // baseURL: 'https://4f7f-2803-d100-ea40-82d-b427-3254-ad2d-eb87.ngrok-free.app/stack-ant-prod/us-central1/fieldservice',
-  baseURL: process.env.REACT_APP_URI_FIELD,
+  baseURL: 'https://238d-200-35-181-90.ngrok-free.app/stack-ant-prod/us-central1/fieldservice',
+  // baseURL: process.env.REACT_APP_URI_FIELD,
 });
 
 export const axiosInstanceShopkeeper = axios.create({
-  // baseURL: "https://4f7f-2803-d100-ea40-82d-b427-3254-ad2d-eb87.ngrok-free.app/stack-ant-prod/us-central1/shopkeeperapi",
-  baseURL: process.env.REACT_APP_URI_SHOPKEEPER,
+  baseURL: "https://238d-200-35-181-90.ngrok-free.app/stack-ant-prod/stack-ant-prod/us-central1/shopkeeperapi",
+  // baseURL: process.env.REACT_APP_URI_SHOPKEEPER,
   headers: {token: process.env.REACT_APP_SHOPKEEPER_TOKEN},
-}); 
- 
+});
+
 export const customHeadersAuth = async() => {
   const dataUser = await auth().currentUser.getIdTokenResult();
   return ({
     headers: {token: dataUser.token, 'Cache-Control': 'no-cache'},
   }); 
-}; 
+};
 
 export const customHeadersWithoutAuth = () => {
   return ({
